5aa087f1c6ae1ac9ef4ed5f860d5caf1e7a2c09b
Dylan <dylanc021684@gmail.com>
2025-09-18 11:21:04 -0300

    solucion gracias

--- src/components/CardProducto.jsx
+++ src/components/CardProducto.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import '../Styles/cardProducto.css';
+import '../styles/CardProducto.css';
 
 const CardProducto = ({ producto, agregarAlCarrito }) => {
   const [cantidad, setCantidad] = useState(1);
